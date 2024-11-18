0661dc3563b752d121329795ff9bbe1d39f63867
BrianMath <brianmatheus10@gmail.com>
2024-11-18 16:18:13 -0300

    neotree shortcut to focus on files

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -9,8 +9,22 @@ return {
 		},
 		opts = function()
 			vim.keymap.set('n', '<leader><leader>', ':Neotree filesystem toggle left<CR>', {})
+			vim.keymap.set('n', '<leader>ee', ':Neotree filesystem focus left<CR>', {})
 			vim.keymap.set('n', '<leader>eg', ':Neotree git_status toggle float<CR>', {})
 			vim.keymap.set('n', '<leader>eb', ':Neotree buffers toggle float<CR>', {})
+
+			local neotree = require("neo-tree")
+			neotree.setup({
+				source_selector = {
+					winbar = false,
+					statusline = true,
+					sources = {
+						{ source = "filesystem" },
+						{ source = "buffers" },
+						{ source = "git_status" }
+					}
+				}
+			})
 		end
 	}
 }
