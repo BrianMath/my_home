690ec6b1622aabd0a6eb21581cf0a63c87edc32b
BrianMath <brianmatheus10@gmail.com>
2025-08-21 08:55:55 -0300

    opam configs

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -7,9 +7,7 @@ return {
 			local opts = { noremap = true, silent = true }
 		
 			-- Toggle terminal on/off
-			map('n', "<C-\\><Down>", '<Cmd>ToggleTerm<CR>', opts)
-			map('n', "<C-\\><Right>", '<Cmd>ToggleTerm direction=vertical<CR>', opts)
-			map('n', "<C-\\><Up>", '<Cmd>ToggleTerm direction=float<CR>', opts)
+			map('n', "<C-\\>", '<Cmd>ToggleTerm<CR>', opts)
 		end
 	}
 }
